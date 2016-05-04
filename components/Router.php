265eeb850918940e2315f94b6e25586e9696bbc0
Svatok <s_s_s@ua.fm>
2016-05-04 23:49:45 +0300

    Update Router.php

--- components/Router.php
+++ components/Router.php
@@ -4,10 +4,12 @@ class Router {
   private $routes;
   
   public function __construct(){
-    
+    $routesPath=ROOT.'/config/routes.php';
+    $this->routes=include($routesPath);
   }
   
   public function run(){
+    print_r($this->routes);
     echo 'Class Router, method Run';
   }
 }
