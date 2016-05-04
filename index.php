3556f3c86837cc6d9bfbd06f9ad3abe7c67ba43a
Svatok <s_s_s@ua.fm>
2016-05-04 23:38:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 // 2. Connecting sustem files
-define('ROOT',dirname(_FILE_));
+define('ROOT',dirname(__FILE__));
 require_once(ROOT.'/components/Router.php');
 
 // 3. Connect to DB
