edcdcba817e421429529873bb2fda3062c7bf5a8
Svatok <s_s_s@ua.fm>
2016-05-05 22:25:18 +0300

    Create db_params.php

---
+++ config/db_params.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'host'=>'ec2-54-228-219-2.eu-west-1.compute.amazonaws.com',
+    'dbname'=>'d54a0dd3p463b',
+    'user'=>'ozothppcrrbtpt',
+    'password'=>'QxLJ0IoqGCpetHKE4yJXm1rQXy',
+);
+
+?>
