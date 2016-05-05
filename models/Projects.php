417766f665c66f076a04e1effcd837f1c0d77dd4
Svatok <s_s_s@ua.fm>
2016-05-05 22:26:20 +0300

    Update Projects.php

--- models/Projects.php
+++ models/Projects.php
@@ -3,12 +3,8 @@ class Projects{
 
   public static function getProjectsList(){
     
-    $host='ec2-54-228-219-2.eu-west-1.compute.amazonaws.com';
-    $dbname='d54a0dd3p463b';
-    $user='ozothppcrrbtpt';
-    $password='QxLJ0IoqGCpetHKE4yJXm1rQXy';
-    $db = new PDO('pgsql:user='.$user.' dbname='.$dbname.' password='.$password.' host='.$host);
-  
+    $db=Db::getConnection();
+    
     $projectsList=array();
     
     $result=$db->query('SELECT id, name FROM projects');
