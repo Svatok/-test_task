59b3100595993a8486298f13e26eaaadb7a08e13
Svatok <s_s_s@ua.fm>
2016-05-05 22:10:52 +0300

    Update ProjectsController.php

--- controllers/ProjectsController.php
+++ controllers/ProjectsController.php
@@ -1,8 +1,17 @@
 <?php
+
+include_once ROOT.'/models/Projects.php';
+
 class ProjectsController{
   
   public function actionIndex(){
-    echo 'ProjectController actionIndex';
+    $projectsList=array();
+    $projectsList=Projects::getProjectsList();
+    
+    echo '<pre>';
+    print_r($projectsList);
+    echo '</pre>';
+    
     return true;
     
   }
