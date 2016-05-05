4ae4df4e5ce6ba1065df23fc0cc5d48c8f120c39
Svatok <s_s_s@ua.fm>
2016-05-05 23:22:01 +0300

    Update index.php

--- views/projects/index.php
+++ views/projects/index.php
@@ -7,7 +7,12 @@
 </head>
 
 <body>
-  ss
+  <?php foreach ($projectsList as $projectsItem):?>
+  <div>
+    <h2>ID: <?php echo $projectsItem['id'];?></h2>
+    <p>Project: <?php echo $projectsItem['name'];?></p>
+  </div>
+  <?php endforeach; ?>
 </body>
 
 </html>
