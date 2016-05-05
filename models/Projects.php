3bddb74f2b32a81eb98fccae5f148b934410feb7
Svatok <s_s_s@ua.fm>
2016-05-05 22:09:53 +0300

    Update Projects.php

--- models/Projects.php
+++ models/Projects.php
@@ -2,6 +2,25 @@
 class Projects{
 
   public static function getProjectsList(){
+    
+    $host='ec2-54-228-219-2.eu-west-1.compute.amazonaws.com';
+    $dbname='d54a0dd3p463b';
+    $user='ozothppcrrbtpt';
+    $password='QxLJ0IoqGCpetHKE4yJXm1rQXy';
+    $db = new PDO('pgsql:user='.$user.' dbname='.$dbname.' password='.$password.' host='.$host);
+  
+    $projectsList=array();
+    
+    $result=$db->query('SELECT id, name FROM projects');
+    
+    $i=0;
+    while($row=$result->fetch()){
+      $projectsList[$i]['id']=$row['id'];
+      $projectsList[$i]['name']=$row['name'];
+      $i++;
+    }
+  
+    return $projectsList;
   }
 
 }
