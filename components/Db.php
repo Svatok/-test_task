0d17a4ca2487491d1345652025bff18c1229c3a7
Svatok <s_s_s@ua.fm>
2016-05-05 22:22:21 +0300

    Create Db.php

---
+++ components/Db.php
@@ -0,0 +1,13 @@
+<?pp
+class Db {
+
+  public static function getConnection(){
+    $paramsPath=ROOT.'/config/db_params.php';
+    $params=include($paramsPath);
+    
+    $db=new PDO('pgsql:user='.$params['user'].' dbname='.$params['dbname'].' password='.$params['password'].' host='.$params['host']);
+    
+    return $db;
+  }
+}
+?>
