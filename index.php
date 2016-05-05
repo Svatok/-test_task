5e1f3e2e218b0163488d56c14c78640d39f273da
Svatok <s_s_s@ua.fm>
2016-05-05 22:57:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
-// 2. Connecting sustem files
+// 2. Connecting system files
 define('ROOT',dirname(__FILE__));
 require_once(ROOT.'/components/Router.php');
 
 // 3. Connect to DB
+require_once(ROOT.'/components/Db.php');
+
 
 // 4. Call Router
 $router=new Router();
