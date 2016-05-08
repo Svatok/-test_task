e2787fa65942a5e104f078880a69733ef6162fab
Svatok <s_s_s@ua.fm>
2016-05-09 00:42:24 +0300

    Update register.php

--- views/user/register.php
+++ views/user/register.php
@@ -1,7 +1,7 @@
 <div>
   <form action="#" method="post">
-    <input type="text" name="email" placeholder="E-mail"/>
-    <input type="password" name="password" placeholder="Pass"/>
+    <input type="text" name="email" placeholder="E-mail" value="<?php echo $email; ?>"/>
+    <input type="password" name="password" placeholder="Pass" value="<?php echo $password; ?>"/>
     <input type="submit" name="submit" value="Registration"/>
   </form>
 </div>
