c83dea1d7710b46225910b51389a62384893caf1
Svatok <s_s_s@ua.fm>
2016-05-08 23:26:33 +0300

    Update index.php

--- views/projects/index.php
+++ views/projects/index.php
@@ -1,8 +1,8 @@
-<? php include ROOT.'/views/layouts/header.php'; ?>
+<?php include ROOT.'/views/layouts/header.php'; ?>
   <?php foreach ($projectsList as $projectsItem):?>
   <div>
     <h2>ID: <?php echo $projectsItem['id'];?></h2>
     <p>Project: <?php echo $projectsItem['name'];?></p>
   </div>
   <?php endforeach; ?>
-<? php include ROOT.'/views/layouts/footer.php'; ?>
+<?php include ROOT.'/views/layouts/footer.php'; ?>
