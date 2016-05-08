2f7c759b4709c0b73c46d6696b8340addac4a7b1
Svatok <s_s_s@ua.fm>
2016-05-09 00:07:02 +0300

    Create Autoload.php

---
+++ components/Autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+function __autoload($class_name){
+
+  //list all the class directories in array
+  $array_paths = array(
+    '/models/',
+    '/components/'
+  );
+  
+  foreach ($array_paths as $path) {
+    $path=ROOT.$path.$class_name.'.php';
+    if (is_file($path)){
+      include_once $path;
+    }
+  }
+}
+?>
