ff553f3d43a87e85f3630bb336f2751dc70f0535
Svatok <s_s_s@ua.fm>
2016-05-09 00:29:00 +0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,8 @@
 <?php
 return array (
   'projects/([0-9]+)' => 'projects/tasks/$1',
+  'projects' => 'projects/index',
+  'user/register' => 'user/register',
   '' => 'projects/index', //actionIndex in ProjectsController
 );
 ?>
