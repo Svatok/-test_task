612f09790b4715ccf68ae62b9520cc2810da8f64
Svatok <s_s_s@ua.fm>
2016-05-09 00:55:07 +0300

    Update UserController.php

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -11,6 +11,20 @@ class UserController{
       $email=$_POST['email'];
       $password=$_POST['password'];
       
+      $errors=false;
+      
+      if (User::checkEmail($email)){
+        echo 'em-ok';
+      }else{
+        $errors[]='Wrong e-mail!';
+      }
+      if (User::checkPassword($password)){
+        echo 'pass-ok';
+      }else{
+        $errors[]='Password have to more then 4 symbols!';
+      }      
+      
+      
     }
     
     require_once(ROOT.'/views/user/register.php');
