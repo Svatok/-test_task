f56b64d9963ae5561997b6cee961f598551dd060
Svatok <s_s_s@ua.fm>
2016-05-09 00:22:24 +0300

    Create register.php

---
+++ views/user/register.php
@@ -0,0 +1,7 @@
+<div>
+  <form action="#" method="post">
+    <input type="text" placeholder="E-mail"/>
+    <input type="password" placeholder="Pass"/>
+    <button type="submit">Registration</button>
+  </form>
+</div>
