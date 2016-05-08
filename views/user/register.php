f567d1fae738293d84a480fbaf5cf6912814ef20
Svatok <s_s_s@ua.fm>
2016-05-09 00:59:27 +0300

    Update register.php

--- views/user/register.php
+++ views/user/register.php
@@ -1,4 +1,11 @@
 <div>
+  <?php if (isset($errors) && is_array($errors)): ?>
+    <ul>
+      <?php foreach ($errors as $error): ?>
+        <li> - <?php echo $error; ?></li>
+      <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
   <form action="#" method="post">
     <input type="text" name="email" placeholder="E-mail" value="<?php echo $email; ?>"/>
     <input type="password" name="password" placeholder="Pass" value="<?php echo $password; ?>"/>
