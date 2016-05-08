fadcd4266e47cb4e735a52dd8937076711fa1a97
Svatok <s_s_s@ua.fm>
2016-05-09 00:50:35 +0300

    Update User.php

--- models/User.php
+++ models/User.php
@@ -4,6 +4,18 @@ class User{
   public static function register() {
   
   }
+  public static function checkEmail($email) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+      return true;
+    }
+    return false;
+  }
+  public static function checkPassword($password) {
+    if (strlen($password)>=4){
+      return true;
+    }
+    return false;
+  }
 }
 
 ?>
