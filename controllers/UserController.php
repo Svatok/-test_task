974b4b22c3c696609113cd7ddaefb81d7124db11
Svatok <s_s_s@ua.fm>
2016-05-09 00:38:39 +0300

    Update UserController.php

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,6 +2,19 @@
 
 class UserController{
   public function actionRegister(){
+    if (isset($_POST['submit'])){
+      $email=$_POST['email'];
+      $password=$_POST['password'];
+      
+      if (isset($email)){
+        echo $email;
+      }
+    if (isset($password)){
+        echo $password;
+      }
+      
+    }
+    
     require_once(ROOT.'/views/user/register.php');
     
     return true;
