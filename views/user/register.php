0b97f60140ddc477ad04d39521bfe396dd74dd5e
Svatok <s_s_s@ua.fm>
2016-05-09 01:38:41 +0300

    Update register.php

--- views/user/register.php
+++ views/user/register.php
@@ -1,14 +1,18 @@
-<div>
-  <?php if (isset($errors) && is_array($errors)): ?>
-    <ul>
-      <?php foreach ($errors as $error): ?>
-        <li> - <?php echo $error; ?></li>
-      <?php endforeach; ?>
-    </ul>
-  <?php endif; ?>
-  <form action="#" method="post">
-    <input type="text" name="email" placeholder="E-mail" value="<?php echo $email; ?>"/>
-    <input type="password" name="password" placeholder="Pass" value="<?php echo $password; ?>"/>
-    <input type="submit" name="submit" value="Registration"/>
-  </form>
-</div>
+<?php if ($result): ?>
+  <div>Are you registered!</div>
+<?php else: ?>
+  <div>
+    <?php if (isset($errors) && is_array($errors)): ?>
+      <ul>
+        <?php foreach ($errors as $error): ?>
+          <li> - <?php echo $error; ?></li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; ?>
+    <form action="#" method="post">
+      <input type="text" name="email" placeholder="E-mail" value="<?php echo $email; ?>"/>
+      <input type="password" name="password" placeholder="Pass" value="<?php echo $password; ?>"/>
+      <input type="submit" name="submit" value="Registration"/>
+    </form>
+  </div>
+<?php endif; ?>
