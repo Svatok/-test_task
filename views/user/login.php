5ef89ed75c32b94ddbcf7fc256c9d49f7cf53aa4
Svatok <s_s_s@ua.fm>
2016-05-10 22:51:11 +0300

    Create login.php

---
+++ views/user/login.php
@@ -0,0 +1,14 @@
+<div>
+    <?php if (isset($errors) && is_array($errors)): ?>
+      <ul>
+        <?php foreach ($errors as $error): ?>
+          <li> - <?php echo $error; ?></li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; ?>
+    <form action="#" method="post">
+      <input type="text" name="email" placeholder="E-mail" value="<?php echo $email; ?>"/>
+      <input type="password" name="password" placeholder="Pass" value="<?php echo $password; ?>"/>
+      <input type="submit" name="submit" value="Registration"/>
+    </form>
+  </div>
