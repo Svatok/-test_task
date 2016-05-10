3ea849749a0d4dea936328cf14074a66b0b5cb38
Svatok <s_s_s@ua.fm>
2016-05-10 22:47:11 +0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,7 @@ return array (
   'projects/([0-9]+)' => 'projects/tasks/$1',
   'projects' => 'projects/index',
   'user/register' => 'user/register',
+  'user/login' => 'user/login',
   '' => 'projects/index', //actionIndex in ProjectsController
 );
 ?>
