b69e6ff0e49edf2bd2032243677a913a62037203
Svatok <s_s_s@ua.fm>
2016-05-10 23:00:29 +0300

    Update User.php

--- models/User.php
+++ models/User.php
@@ -12,6 +12,30 @@ class User{
     return $result->execute();
   }
   
+  public static function checkUserData($email, $password){
+    $db=Db::getConnection();
+    
+    $sql='SELECT * FROM user_table WHERE email=:email AND pass=:password';
+    
+    $result=$db->prepare($sql);
+    $result->bindParam(':email',$email,PDO::PARAM_STR);
+    $result->bindParam(':password',$password,PDO::PARAM_STR);
+    $result->execute();
+    
+    $user=$result->fetch();
+    if ($user){
+      return $user['id'];
+    }
+    
+    return false;
+    
+  }
+  
+  public static function auth($userId){
+    session_start();
+    $_SESSION['user']=$userId;
+  }
+  
   public static function checkEmail($email) {
     if (filter_var($email, FILTER_VALIDATE_EMAIL)){
       return true;
