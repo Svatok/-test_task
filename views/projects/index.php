04bdf607d949d5656388e299e00f934790a0612f
Svatok <s_s_s@ua.fm>
2016-05-12 23:28:58 +0300

    Update index.php

--- views/projects/index.php
+++ views/projects/index.php
@@ -1,9 +1,11 @@
 <?php include ROOT.'/views/layouts/header.php'; ?>
-  <div>
+  <div id="div_project_<?php echo $projectsItem['id'];?>">
       <ul>
       <?php foreach ($projectsList as $projectsItem):?>
          <li><a href="/projects/<?php echo $projectsItem['id'];?>"><?php echo $projectsItem['name'];?></li>
       <?php endforeach; ?>
       </ul>
+      <div id="div_tasks_<?php echo $projectsItem['id'];?>">
+      </div>
   </div>
 <?php include ROOT.'/views/layouts/footer.php'; ?>
