2e3058b82371830e2a66b5b70a1d4beb4aefac48
Svatok <s_s_s@ua.fm>
2016-05-13 00:32:50 +0300

    Update tasks_list.php

--- views/projects/tasks_list.php
+++ views/projects/tasks_list.php
@@ -1,6 +1,5 @@
+<ul>
   <?php foreach ($tasksList as $tasksItem):?>
-  <div>
-    <h2>ID: <?php echo $tasksItem['id'];?></h2>
-    <p>Task: <?php echo $tasksItem['name'];?></p>
-  </div>
+  <li>Task: <?php echo $tasksItem['name'];?></li>
   <?php endforeach; ?>
+</ul>
