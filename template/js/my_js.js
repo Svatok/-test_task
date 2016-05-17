e760476d229fe95ad31f8fd3b730add501aeaa70
Svatok <s_s_s@ua.fm>
2016-05-17 14:10:29 +0300

    Create my_js.js

---
+++ template/js/my_js.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".project").click(function (){
+      var id=$(this).attr("data-id");
+      if ($("#div_tasks_"+id).is(':empty')){
+          $.post("/projects/"+id, {}, function (data){
+            $("#div_tasks_"+id).html(data);
+          });
+      } else {
+          $("#div_tasks_"+id).empty();
+      }
+      return false;
+    });
+}); 
