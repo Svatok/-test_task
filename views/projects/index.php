40ff047b399c136ac7a68bbce8561430453a3c24
Svatok <s_s_s@ua.fm>
2016-05-17 12:55:20 +0300

    Update index.php

--- views/projects/index.php
+++ views/projects/index.php
@@ -2,8 +2,7 @@
     foreach ($projectsList as $projectsItem):?>
     <div id="div_project_<?php echo $projectsItem['id'];?>">
       <h2><a href="/projects/<?php echo $projectsItem['id'];?>" data-id="<?php echo $projectsItem['id'];?>" class="project"><?php echo $projectsItem['name'];?></a></h2>
-        <div id="div_tasks_<?php echo $projectsItem['id'];?>">
-        </div>
+        <div id="div_tasks_<?php echo $projectsItem['id'];?>"></div>
     </div>
     <?php endforeach; 
 include ROOT.'/views/layouts/footer.php'; ?>
