3c42260b8fc7b831d7e9e933d8bbd2c35248db64
Svatok <s_s_s@ua.fm>
2016-05-17 12:04:50 +0300

    Update tasks_list.php

--- views/projects/tasks_list.php
+++ views/projects/tasks_list.php
@@ -1,5 +1,24 @@
 <ul>
   <?php foreach ($tasksList as $tasksItem):?>
-  <li><?php echo $tasksItem['name'];?></li>
+  <li>
+    <form action="" method="post" class="task_form">
+      <?php if ($tasksItem['status']==1): ?>
+        <input type="checkbox" checked>
+      <?php else: ?>
+        <input type="checkbox">
+      <?php endif; ?>
+      <input type="text" id="<?php echo $tasksItem['id'];?>" value="<?php echo $tasksItem['name'];?>"/>
+      <div class="out_edit">
+        <a href="" class="up_task">Up</a>
+        <a href="" class="down_task">Down</a>
+        <a href="" class="edit">Edit</a>
+        <a href="" class="del">Del</a>
+      </div>
+      <div class="in_edit">
+        <a href="" class="save">Save</a>
+        <a href="" class="cansel">Cansel</a>
+      </div>
+    </form>
+  </li>
   <?php endforeach; ?>
 </ul>
