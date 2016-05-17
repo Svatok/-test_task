aee83496de7d85fbea4f9716317ab139465c4e4d
Svatok <s_s_s@ua.fm>
2016-05-18 00:31:44 +0300

    Update Projects.php

--- models/Projects.php
+++ models/Projects.php
@@ -26,7 +26,7 @@ class Projects{
     $db=Db::getConnection();
     
     $tasksList=array();
-    $result=$db->query('SELECT * FROM tasks WHERE project_id='.$id.' ORDER BY priority');
+    $result=$db->query('SELECT * FROM tasks WHERE project_id='.$id.' ORDER BY priority DESC');
     
     $i=0;
     while($row=$result->fetch()){
