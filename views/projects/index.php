60ac34190fb2fec3cffff83f1e8eac5f80246e33
Svatok <s_s_s@ua.fm>
2016-05-18 16:27:30 +0300

    Update index.php

--- views/projects/index.php
+++ views/projects/index.php
@@ -1,10 +1,8 @@
 <?php include ROOT.'/views/layouts/header.php';
     foreach ($projectsList as $projectsItem):?>
-    <textarea data-autoresize placeholder="Type some text"></textarea>
     <div id="div_project_<?php echo $projectsItem['id'];?>">
       <h2><a href="/projects/<?php echo $projectsItem['id'];?>" data-id="<?php echo $projectsItem['id'];?>" class="project"><?php echo $projectsItem['name'];?></a></h2>
         <div id="div_tasks_<?php echo $projectsItem['id'];?>" class="div_tasks"></div>
     </div>
     <?php endforeach; 
 include ROOT.'/views/layouts/footer.php'; ?>
-<button type="button" id="button1">Add1</button>
