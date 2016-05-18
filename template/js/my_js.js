38994dc093a252b7e4104404037d0a19049a5916
Svatok <s_s_s@ua.fm>
2016-05-18 23:45:25 +0300

    Update my_js.js

--- template/js/my_js.js
+++ template/js/my_js.js
@@ -2,8 +2,11 @@ $(document).ready(function () {
     $('.div_tasks').on('keyup input', '.input_text', function(){
         var offset = this.offsetHeight - this.clientHeight;
         var h= this.offsetHeight;
+            $(this).scroll(function(){
+                alert('Был осуществлен Scroll');
+            });
         if (this.scrollHeight != this.offsetHeight){
-            alert(this.scrollHeight+'!='+this.offsetHeight);
+           // alert(this.scrollHeight+'!='+this.offsetHeight);
             $(this).css('height', 'auto').css('height', this.scrollHeight + offset);
         }
      /*   $(this).css({
