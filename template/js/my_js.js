852879bbb84f031f6ce67c8da0cea50542c924e6
Svatok <s_s_s@ua.fm>
2016-05-18 14:57:03 +0300

    Update my_js.js

--- template/js/my_js.js
+++ template/js/my_js.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
-
+    var max_height = 100;
+    
     $('.div_tasks').on('keyup input', '.input_text', function(){
-        alert('ss');
+       // alert('ss');
+        var offset = this.offsetHeight - this.clientHeight;
+        this.css('height', 'auto').css('height', Math.min(max_height, this.scrollHeight + offset));
     });
 
     $('.div_tasks').on('click', '.del', function(e){
