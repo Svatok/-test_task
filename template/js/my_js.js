7fd4afd67b3bbf924426b1d190f72c63de52f6d8
Svatok <s_s_s@ua.fm>
2016-05-18 15:51:14 +0300

    Update my_js.js

--- template/js/my_js.js
+++ template/js/my_js.js
@@ -97,11 +97,14 @@ $(document).ready(function () {
     $(".project").click(function (){
       var id=$(this).attr("data-id");
       if ($("#div_tasks_"+id).is(':empty')){
-          $.post("/projects/"+id, {}, function (data){
+        $.post("/projects/"+id, {}, function (data){
             $("#div_tasks_"+id).html(data);
-          });
+        });
+        $(".input_text").each(function() {
+           $(this).css('background', 'green'); 
+        });
       } else {
-          $("#div_tasks_"+id).empty();
+        $("#div_tasks_"+id).empty();
       }
       return false;
     });
