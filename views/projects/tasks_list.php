26f1596e064c56a5d8bc5b377790693351531131
Svatok <s_s_s@ua.fm>
2016-05-19 16:00:17 +0300

    Update tasks_list.php

--- views/projects/tasks_list.php
+++ views/projects/tasks_list.php
@@ -7,15 +7,17 @@
         <input type="checkbox">
       <?php endif; ?>
       <div class="div_task_text"><?php echo $tasksItem['name'];?></div>
-      <div class="out_edit">
-        <a href="" class="up_task">Up</a>
-        <a href="" class="down_task">Down</a>
-        <a href="" class="edit">Edit</a>
-        <a href="" class="del">Del</a>
-      </div>
-      <div class="in_edit">
-        <a href="" class="save">Save</a>
-        <a href="" class="cancel">Cancel</a>
+      <div class="edit_buttons">
+        <div class="out_edit">
+          <a href="" class="up_task">Up</a>
+          <a href="" class="down_task">Down</a>
+          <a href="" class="edit">Edit</a>
+          <a href="" class="del">Del</a>
+        </div>
+        <div class="in_edit">
+          <a href="" class="save">Save</a>
+          <a href="" class="cancel">Cancel</a>
+        </div>
       </div>
   </li>
   <?php endforeach; ?>
