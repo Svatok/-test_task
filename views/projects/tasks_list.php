335b7586b471a702d865e7963075ea3692931901
Svatok <s_s_s@ua.fm>
2016-05-22 19:21:06 +0300

    Update tasks_list.php

--- views/projects/tasks_list.php
+++ views/projects/tasks_list.php
@@ -3,11 +3,11 @@
     <td>
       +
     </td>
-    <td>
-      <input type="text" placeholder="Start typing here to create a task..."></input>
+    <td class="add_task_input"> 
+      <input type="text" width="90%" placeholder="Start typing here to create a task..."></input>
     </td>
-    <td>
-      <a href="#" class="add_task_button">Add Task</a>
+    <td class="add_task_button">
+      <a href="#">Add Task</a>
     </td>
   </tr>
   
