9b973133fae63283dcb39d841885334e6ef877f7
Svatok <s_s_s@ua.fm>
2016-05-26 17:38:20 +0300

    Update Tasks.php

--- models/Tasks.php
+++ models/Tasks.php
@@ -1,6 +1,7 @@
 
 <?php
 class Tasks{
+  $allowedStatuses = array(0, 1, 2); // 0 - In work, 1 - Done, 2 - Delete
   
   public static function getTaskData($id){
     
@@ -14,5 +15,30 @@ class Tasks{
   
     return $taskData;
   }
+  
+  public static function editTaskData($prop, $val, $id){
+    
+    $id=intval($id);
+    $db=Db::getConnection();
+    
+    $sql = "UPDATE tasks SET ".$prop." = :val WHERE ID = :id";
+    $stmt = $pdo->prepare($sql);                                  
+    $stmt->bindParam(':val',$val, PDO::PARAM_STR);       
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);   
+    if ($stmt->execute()){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
+  public static function checkStatus($val){
+    if (in_array($val, $allowedStatuses)){
+      return true;
+    }else{
+      return false;
+    }
+  }  
+  
 }
 ?>
