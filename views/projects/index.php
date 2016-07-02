8967c5f2d2aa2afeb8c5a00d41513248d6061a53
Svatok <s_s_s@ua.fm>
2016-07-02 14:00:10 +0300

    Update index.php

--- views/projects/index.php
+++ views/projects/index.php
@@ -3,24 +3,7 @@
     <div id="div_project_<?php echo $projectsItem['id'];?>">
       <h2><a href="/projects/<?php echo $projectsItem['id'];?>" data-id="<?php echo $projectsItem['id'];?>" class="project"><?php echo $projectsItem['name'];?></a></h2>
         <table id="container_tasks_<?php echo $projectsItem['id'];?>" class="container_tasks">
-          <tr class="project_tr" id="project_<?php echo $projectsItem['id'];?>">
-            <td>
-              !!!
-            </td>
-            <td class="div_project_container"> 
-              <div class="div_project_text"><?php echo $projectsItem['name'];?></div>
-            </td>
-            <td class="div_edit_buttons">
-                <div class="out_edit">
-                  <a href="" class="edit">Edit</a>
-                  <a href="" class="del">Del</a>
-                </div>
-                <div class="in_edit">
-                  <a href="" class="save">Save</a>
-                  <a href="" class="cancel">Cancel</a>
-                </div>
-            </td>
-          </tr>
+
             <tr class="add_task">
                 <td>
                   +
