9db08a80bdf6b2592307be662f3584355a72b9bd
Svatok <s_s_s@ua.fm>
2016-07-02 14:03:08 +0300

    Update index.php

--- views/projects/index.php
+++ views/projects/index.php
@@ -3,18 +3,24 @@
     <div id="div_project_<?php echo $projectsItem['id'];?>">
       <h2><a href="/projects/<?php echo $projectsItem['id'];?>" data-id="<?php echo $projectsItem['id'];?>" class="project"><?php echo $projectsItem['name'];?></a></h2>
         <table id="container_tasks_<?php echo $projectsItem['id'];?>" class="container_tasks">
-
-            <tr class="add_task">
-                <td>
-                  +
-                </td>
-                <td class="add_task_input"> 
-                  <input type="text" placeholder="Start typing here to create a task..."></input>
-                </td>
-                <td class="add_task_button">
-                  <a href="#" class="save_add">Add Task</a>
-                </td>
-              </tr>
+          <tr priority="100" class="task" id="task_100">
+              <td class="div_check">
+                <input type="checkbox" class="task_status">
+              </td>
+              <td class="div_task_container"><div class="div_task_text"><?php echo $projectsItem['name'];?></div></td>
+              <td class="div_edit_buttons">
+                <div class="out_edit">
+                  <a href="" class="up_task">Up</a>
+                  <a href="" class="down_task">Down</a>
+                  <a href="" class="edit">Edit</a>
+                  <a href="" class="del">Del</a>
+                </div>
+                <div class="in_edit">
+                  <a href="" class="save">Save</a>
+                  <a href="" class="cancel">Cancel</a>
+                </div>
+              </td>
+          </tr>
         </table>
     </div>
     <?php endforeach; 
