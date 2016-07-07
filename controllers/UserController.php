6b637debc8efe779b7af2bcc6b5529c171b7454e
Svatok <s_s_s@ua.fm>
2016-07-08 00:27:45 +0300

    Update UserController.php

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -4,8 +4,8 @@ class UserController{
 
 
   public function actionRegister(){
-    $email='';
-    $password='';
+    $email='test@test.com';
+    $password='test';
     
     if (isset($_POST['submit'])){
       $email=$_POST['email'];
