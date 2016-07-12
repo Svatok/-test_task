8a37385dc79b826f3a03bd6cc5d25f740503df0f
Svatok <s_s_s@ua.fm>
2016-07-12 09:34:46 +0300

    Update ProjectsController.php

--- controllers/ProjectsController.php
+++ controllers/ProjectsController.php
@@ -15,7 +15,7 @@ class ProjectsController{
     
   }
   
-    public function actionTasks($params){
+  public function actionTasks($params){
     $tasksList=array();
     $tasksList=Projects::getTasksList($params[0]);
     
@@ -24,5 +24,35 @@ class ProjectsController{
     return true;
     
   }
+
+  public function actionEdit($params){
+    $projectText='';
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $userId=User::checkLogged();
+      $projectData=Projects::getProjectData($params[0]);
+      $errors=false;
+      $updateData=array();
+      
+      if (isset($_POST['name'])){
+        $projectText=Projects::clean($_POST['name']);
+        if (Projects::checkText($projectText)){
+          if ($projectData['name']!=$projectText){
+            if(Projects::editProjectData('name', $projectText, $params[0])){
+              $updateData['name']='Success:Task was changed!';
+            }else{
+              $updateData['name']='Error:Database Error: Task is not changed';
+            }
+          }          
+        }else{
+          $updateData['name']='Error:Invalid text of task!';
+        }
+      }
+    }
+    echo json_encode($updateData);
+    
+    return true;
+  }
+  
 }
 ?>
