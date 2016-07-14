236be4b1d6e809e7b324e8587c6fa9b113a47621
Svatok <s_s_s@ua.fm>
2016-07-14 16:45:46 +0300

    Update login.php

--- views/user/login.php
+++ views/user/login.php
@@ -1,21 +1,15 @@
-<?php include ROOT.'/views/layouts/header.php';?>
-<div class="parent_enter">
-<form action="" method="post" class="form-container">
-        <?php if (isset($errors) && is_array($errors)): ?>
+
+<div>
+    <?php if (isset($errors) && is_array($errors)): ?>
       <ul>
         <?php foreach ($errors as $error): ?>
           <li> - <?php echo $error; ?></li>
         <?php endforeach; ?>
       </ul>
     <?php endif; ?>
-<div class="form-title"><h2>Sign up</h2></div>
-<div class="form-title">Email</div>
-<input class="form-field" type="text" name="name" placeholder="E-mail" /><br />
-<div class="form-title" type="password" name="password" placeholder="Password">Password</div>
-<input class="form-field" type="text" name="email" /><br />
-<div class="submit-container">
-<input class="submit-button" type="submit" name="submit" value="Enter" />
-</div>
-</form>
-</div>
-<?php include ROOT.'/views/layouts/footer.php'; ?>
+    <form action="#" method="post">
+      <input type="text" name="email" placeholder="E-mail" value="<?php echo $email; ?>"/>
+      <input type="password" name="password" placeholder="Pass" value="<?php echo $password; ?>"/>
+      <input type="submit" name="submit" value="Enter"/>
+    </form>
+  </div>
