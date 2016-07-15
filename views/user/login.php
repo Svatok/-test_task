e250b93be32dc46862724754f8fb78fe02597e01
Svatok <s_s_s@ua.fm>
2016-07-15 14:12:43 +0300

    Update login.php

--- views/user/login.php
+++ views/user/login.php
@@ -8,9 +8,9 @@
     <?php endif; ?>
     <div class="form-title"><h2>Sign up</h2></div>
     <div class="form-title">Email</div>
-    <input class="form-field" type="text" name="name" placeholder="E-mail" /><br />
+    <input class="form-field" type="text" name="name" placeholder="E-mail" value="test@test.com" /><br />
     <div class="form-title" type="password" name="password" placeholder="Password">Password</div>
-    <input class="form-field" type="text" name="email" /><br />
+    <input class="form-field" type="text" name="email" value="test" /><br />
     <div class="submit-container">
         <input class="submit-button" type="submit" name="submit" value="Enter" />
     </div>
