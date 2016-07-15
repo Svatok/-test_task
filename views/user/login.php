ba8e78b7be0290a12eb8f6dd7f967b11d61323c0
Svatok <s_s_s@ua.fm>
2016-07-15 14:24:03 +0300

    Update login.php

--- views/user/login.php
+++ views/user/login.php
@@ -1,11 +1,4 @@
 <form action="/user/login" method="post" class="form-container">
-   <?php if (isset($errors) && is_array($errors)): ?>
-        <ul>
-        <?php foreach ($errors as $error): ?>
-          <li> - <?php echo $error; ?></li>
-        <?php endforeach; ?>
-      </ul>
-    <?php endif; ?>
     <div class="form-title"><h2>Sign up</h2></div>
     <div class="form-title">Email</div>
     <input class="form-field" type="text" name="name" placeholder="E-mail" value="test@test.com" /><br />
