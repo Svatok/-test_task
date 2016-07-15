deb2ada9b0c5e30c2e4fa589582af5db176f1cda
Svatok <s_s_s@ua.fm>
2016-07-15 12:34:00 +0300

    Update index.php

--- views/projects/index.php
+++ views/projects/index.php
@@ -20,29 +20,4 @@
     <h1>SIMPLE TODO LISTS</h1>
     <h2>FROM RUBY GARAGE</h2>
 </div>
-<div class="main_div"> 
-<?php  foreach ($projectsList as $projectsItem):?>
-    <div id="div_project_<?php echo $projectsItem['id'];?>" class="div_project_border">
-        <table id="container_tasks_<?php echo $projectsItem['id'];?>" class="container_tasks">
-          <tr class="project" id="project_<?php echo $projectsItem['id'];?>">
-              <td class="project_icon">
-                !!!
-              </td>
-              <td class="div_project_container"><div class="div_project_text"><?php echo $projectsItem['name'];?></div></td>
-              <td class="div_edit_buttons">
-                <div class="out_edit">
-                  <a href="" class="edit">Edit</a>
-                  <a href="" class="del">Del</a>
-                </div>
-                <div class="in_edit">
-                  <a href="" class="save">Save</a>
-                  <a href="" class="cancel">Cancel</a>
-                </div>
-              </td>
-          </tr>
-        </table>
-    </div>
-    <?php endforeach;?>
-    <div class="add_button_project"><button>Add TODO List</button></div>
-</div>
 <?php include ROOT.'/views/layouts/footer.php'; ?>
