e7ff487624ce12278d15245297a231a5fd26f873
Svatok <s_s_s@ua.fm>
2016-07-15 17:12:45 +0300

    Update login.php

--- views/user/login.php
+++ views/user/login.php
@@ -1,9 +1,9 @@
 <form action="" method="post" class="form-container">
     <div class="form-title"><h2>Sign up</h2></div>
     <div class="form-title">Email</div>
-    <input class="form-field" type="text" name="email" placeholder="E-mail" value="test@test.com" /><br />
+    <input class="form-field" type="text" name="email" id="email" placeholder="E-mail" value="test@test.com" /><br />
     <div class="form-title">Password</div>
-    <input class="form-field" type="password" name="password" placeholder="Password" value="test"/><br />
+    <input class="form-field" type="password" name="password" id="password" placeholder="Password" value="test"/><br />
     <div class="submit-container">
         <input class="submit-button" type="submit" name="submit" value="Enter" />
     </div>
