ae74c5c6391d0228bbdab4a7fecfb494279f5c26
Svatok <s_s_s@ua.fm>
2016-08-01 10:13:37 +0300

    Update my_js.js

--- template/js/my_js.js
+++ template/js/my_js.js
@@ -191,7 +191,8 @@ function ucfirst(str) {
       before_edit=$("#"+id_task_attr+" .div_"+class_tr_attr+"_text").text();
       var div_text=$("#"+id_task_attr+" .div_"+class_tr_attr+"_text");
       divClicked(div_text);
-      $("#"+id_task_attr+" .out_edit").css('display','none');
+     // $("#"+id_task_attr+" .out_edit").css('display','none');
+      $(class_tr_attr+":hover .out_edit").css('display','none');
       $("#"+id_task_attr+" .in_edit").css('display','inline-block');
     });
 // cancel edit mode of text of task or project
@@ -356,6 +357,7 @@ function ucfirst(str) {
                                     editableTextBlurred(textarea_text, true, class_attr);
                                     $("#"+id_task_attr+" .in_edit").hide();
                                     //$("#"+id_task_attr+" .out_edit").css('display','inline-block');
+                                    $(class_attr+":hover .out_edit").css('display','inline-block');
                                     //add project
                                     if (typeof result_data['projectId'] !=="undefined"){
                                         $('#div_project_NEW').attr('id', 'div_project_'+result_data['projectId']);
@@ -383,6 +385,7 @@ function ucfirst(str) {
                editableTextBlurred(textarea_text, false, class_attr);
                 $("#"+id_task_attr+" .in_edit").hide();
                // $("#"+id_task_attr+" .out_edit").css('display','inline-block');
+                $(class_attr+":hover .out_edit").css('display','inline-block');
                 if ($("div").is("#div_project_NEW")){
                     $('#div_project_NEW').remove();
                 }
