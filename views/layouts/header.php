6abdb67d6c946b5b21897ef0cb2865b0dca791cf
Svatok <s_s_s@ua.fm>
2016-08-03 16:38:57 +0300

    Update header.php

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -7,8 +7,11 @@
 <script src="template/js/jquery.js"></script>
 <script src="template/js/my_js.js"></script>
 <script src="template/js/noty-2.3.8/js/noty/packaged/jquery.noty.packaged.min.js"></script>
-<link href="template/js/jquery-ui-1.12.0.custom/jquery-ui.css" rel="stylesheet" type="text/css"/>
-<script src="template/js/jquery-ui-1.12.0.custom/jquery-ui.js"></script>
+<!--<link href="template/js/jquery-ui-1.12.0.custom/jquery-ui.css" rel="stylesheet" type="text/css"/>
+<script src="template/js/jquery-ui-1.12.0.custom/jquery-ui.js"></script> -->
+<link type="text/css"
+    href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/base/jquery-ui.css" rel="stylesheet" />
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 </head>
 
 <body>
