a488e1bba873c5fda491d1e724c37a1de3f3d2b0
Svatok <s_s_s@ua.fm>
2016-08-16 00:56:06 +0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -9,6 +9,7 @@ return array (
   'user/register' => 'user/register',
   'user/login' => 'user/login',
   'user/logout' => 'user/logout',
+  '404' => '404/index',
   '' => 'projects/index', //actionIndex in ProjectsController
 );
 ?>
