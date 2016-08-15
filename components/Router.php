503bca22cbc836ea6b54119a2155b2bcc6fe4edf
Svatok <s_s_s@ua.fm>
2016-08-16 01:20:06 +0300

    Update Router.php

--- components/Router.php
+++ components/Router.php
@@ -58,8 +58,8 @@ class Router {
     	$host = 'https://'.$_SERVER['HTTP_HOST'].'/';
    	header('HTTP/1.1 404 Not Found');
 //	header("Status: 404 Not Found");
-	header('Location:'.$host.'page404');
-	//header('Location: '.ROOT.'/404');
+//	header('Location:'.$host.'page404');
+	header('Location: '.ROOT.'/page404');
   }
 }
 ?>
