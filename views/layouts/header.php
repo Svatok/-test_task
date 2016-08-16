cc61ad50adc4f2b06532d69dd9a9276e279ab4ca
Svatok <s_s_s@ua.fm>
2016-08-17 00:03:37 +0300

    Update header.php

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Projects</title>
+<title>SIMPLE TODO LISTS</title>
 <link href="template/css/style.css" rel="stylesheet" type="text/css" />
 <script src="template/js/jquery.js"></script>
 <script src="template/js/my_js.js"></script>
