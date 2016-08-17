e0ef9a0205e80f102ca511eddbde0df3daa3c6ff
Svatok <s_s_s@ua.fm>
2016-08-17 23:22:13 +0300

    Update page404.php

--- views/layouts/page404.php
+++ views/layouts/page404.php
@@ -1,1 +1,6 @@
-404. Page not found.
+<?php include ROOT.'/views/layouts/header.php';?>
+<div class="block_404">
+  <div class="h1_div">SORRY, THIS PAGE DOESN'T EXISTS</div>
+  <div class="h2_div">TRY GOING TO OUR HOMEPAGE</div>
+</div>
+<?php include ROOT.'/views/layouts/footer.php';?>
