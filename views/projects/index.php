690e825fc7fa8b2d5a2144092a7dd502a0d17b2b
Svatok <s_s_s@ua.fm>
2016-08-17 23:43:34 +0300

    Update index.php

--- views/projects/index.php
+++ views/projects/index.php
@@ -1,9 +1,5 @@
-<?php include ROOT.'/views/layouts/header.php';?>
-<div class="head_div">
-    <div class="h1_div">SIMPLE TODO LISTS</div>
-    <div class="h2_div">FROM RUBY GARAGE</div>
-</div>
-<?php 
+<?php include ROOT.'/views/layouts/header.php';
+
    if (isset($projectsList)){
       include ROOT.'/views/projects/projects_list.php'; 
    }else{
