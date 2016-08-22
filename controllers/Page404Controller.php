b583fdf1252407035b4f4860005698ae0938aac0
Svatok <s_s_s@ua.fm>
2016-08-22 23:24:18 +0300

    Update Page404Controller.php

--- controllers/Page404Controller.php
+++ controllers/Page404Controller.php
@@ -1,7 +1,9 @@
 <?php
 class Page404Controller{
-  
+
+// Controller connection page 404  
   public function actionIndex(){
+    
     require_once(ROOT.'/views/layouts/page404.php');
     
     return true;
