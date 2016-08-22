6c151b2c85d6e46de6e11db2904703a9061ab9a6
Svatok <s_s_s@ua.fm>
2016-08-23 00:35:36 +0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,7 @@ return array (
   'task/add' => 'tasks/add',
   'task/edit/([0-9]+)' => 'tasks/edit/$1',
   'project/edit/([0-9]+)' => 'projects/edit/$1',
-  'projects/([0-9]+)' => 'projects/tasks/$1',
+  'projects/([0-9]+)' => 'tasks/tasks/$1',
   'projects' => 'projects/projects',
   'user/register' => 'user/register',
   'user/login' => 'user/login',
