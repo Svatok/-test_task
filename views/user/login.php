ab3c67b1d1f284e1edaa610fc21424be8e11267c
Svatok <s_s_s@ua.fm>
2016-08-23 00:31:30 +0300

    Update login.php

--- views/user/login.php
+++ views/user/login.php
@@ -1,11 +1,16 @@
 <form action="" method="post" class="form-container">
+    
     <div class="form-title"><h2>Sign In or Sign Up</h2></div>
+    
     <div class="form-title">Email</div>
     <input class="form-field" type="text" name="email" id="email" placeholder="E-mail" value="test@test.com" /><br />
+    
     <div class="form-title">Password</div>
     <input class="form-field" type="password" name="password" id="password" placeholder="Password" value="test"/><br />
+    
     <div class="submit-container">
         <input class="submit-button" type="submit" name="submit" value="Sign In" />
         <input class="reg-button" type="button" name="submit" value="Sign Up" />
     </div>
+    
 </form>
