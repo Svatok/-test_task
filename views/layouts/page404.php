0c830cbf7b6a69bfad85b0c460242063054a21d0
Svatok <s_s_s@ua.fm>
2016-08-23 00:26:15 +0300

    Update page404.php

--- views/layouts/page404.php
+++ views/layouts/page404.php
@@ -1,6 +1,8 @@
 <?php include ROOT.'/views/layouts/header.php';?>
+
 <div class="block_404">
   <div class="h1_div">SORRY, THIS PAGE DOESN'T EXISTS</div>
   <div class="h2_div">TRY GOING TO OUR <a href="/">HOMEPAGE</a></div>
 </div>
+
 <?php include ROOT.'/views/layouts/footer.php';?>
