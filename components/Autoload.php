27d8962cdd0284ba40e2fa418a6c259a6e851e29
Svatok <s_s_s@ua.fm>
2016-08-22 23:13:03 +0300

    Update Autoload.php
    Autoloading classes.

--- components/Autoload.php
+++ components/Autoload.php
@@ -7,7 +7,7 @@ function __autoload($class_name){
     '/models/',
     '/components/'
   );
-  
+  // connect file
   foreach ($array_paths as $path) {
     $path=ROOT.$path.$class_name.'.php';
     if (is_file($path)){
