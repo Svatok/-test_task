b5c1c924954cca67ddcc103d9a0752504dcd7be4
Svatok <s_s_s@ua.fm>
2016-08-24 01:46:30 +0300

    Update my_js.js

--- template/js/my_js.js
+++ template/js/my_js.js
@@ -476,7 +476,64 @@ $(document).ready(function () {
                                         setTimeout(function(){
                                             $('.ui-datepicker').css('z-index', 99999999999999);
                                         }, 0);
-                                    }
+                                    },
+                                    onSelect:
+                                        function(dateText, inst) {
+            
+                                            var container_deadline=$(this).closest('.div_edit_buttons').children(".div_deadline");
+                                            var task=$(this).closest('.task');
+                                            var id_task=task.attr('id').replace(/[^0-9]/gim,'');
+                                            // Check the double-click
+                                            var me_date = $(this);
+                                            if ( me_date.data('requestRunning') ) {
+                                                return;
+                                            }
+            
+                                            // change date by datepicker
+                                            if (dateText!=container_deadline.html()){
+            
+                                                $.ajax({
+                                                    data: {deadline:dateText},
+                                                    url: '/task/edit/'+id_task,
+                                                    method: 'post',
+                                                    beforeSend: function () {
+                                                        me_date.data('requestRunning', true);
+                                                        $('#loader').show();
+                                                    },
+                                                    success: function (data) {
+                                                        if (data){ 
+                                                            var result_data = $.parseJSON(data);
+                                                            var result_errors = false;
+                                                            $.each(result_data, function(index, value){         
+                                                                if (value.replace(/\:.*/, '')=='Error'){
+                                                                    result_errors = true;
+                                                                    noty({
+                                                                        text: value,
+                                                                        type: 'error',
+                                                                        timeout: '1000'
+                                                                    }); 
+                                                                }
+                                                            });
+                                                            if (!result_errors){
+                                                                container_deadline.html(dateText);
+                                                            }
+                                                        }
+                                                    },
+                                                    error: function(){
+                                                        noty({
+                                                            text: 'Can not change the deadline!',
+                                                            type: 'error',
+                                                            timeout: '1000'
+                                                        });  
+                                                    },
+                                                    complete: function(){
+                                                        me_date.data('requestRunning', false);
+                                                        $('#loader').hide();
+                                                    }
+                                                 }); 
+                                                 
+                                            }
+                                        }
                                 });
                                 
                             }
